425c89e842bd762ae49be749b69f947c092deddc
wcharleswilliams <wcwilliams2009@gmail.com>
2026-09-04 19:19:40 +0000

    fixed website and worked on game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,23 +27,34 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
-
+    createPlatform(200,625,100,10);
+    createPlatform(550,550,75,10);
+    createPlatform(275,425,50,10);
+    createPlatform(400,315,50,10);
+    createPlatform(885,590,75,10);
+    createPlatform(1150,490,70,10);
+    createPlatform(910,380,70,10);
+    createPlatform(1150,300,50,5)
+    createPlatform(50,675,50,10);
+     createPlatform(700,400,0.1,150);
     // TODO 3 - Create Collectables
-
-
-
+    createCollectable("steve",1155,200,0.1,1);
+    createCollectable("steve",575,480,0.1,1);
+    createCollectable("steve",1150,430,0.1,1);
     
     // TODO 4 - Create Cannons
+    createCannon("right",780,1);
+    createCannon("right",450,1500);
+    createCannon("top",690,1300);
+    createCannon("top",200,600);
+    createCannon("right",700,300);
+    createCannon("top",1150,1200);
+    createCannon("right",300,1400);
 
-
-    
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
